4a3a63587e4586d6b04ba84e47be2051658aa8dd
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-05-13 22:44:44 -0600

    separate conditional rendering and tags that require calculation to its own component

--- src/components/ShoeCard/ShoeCard.jsx
+++ src/components/ShoeCard/ShoeCard.jsx
@@ -5,12 +5,35 @@ export default function ShoeCard({shoe}) {
   return (
     <article>
         <img src={shoe.imageSrc} alt={shoe.name} />
-        {isNewShoe(shoe.releaseDate) && <span className='bg-blue-950 text-white'>Just Released!</span>}
-        {shoe.salePrice && <span className='bg-red-300'>Sale</span>}
+        <NewTag release={shoe.releaseDate}/>
+        <SalesTag sale={shoe.salePrice}/>
         <h1>{shoe.name}</h1>
-        <p>{formatPrice(shoe.price)}</p>
-        {shoe.salePrice && <p>{formatPrice(shoe.salePrice)}</p>}
-        <p>{`${shoe.numOfColors} ${shoe.numOfColors > 1 ? 'Colors' : 'Color'}`}</p>
+        <ShoePrice price={shoe.price}/>
+        <SalesPrice price={shoe.salePrice}/>
+        <ColorsP colors={shoe.numOfColors}/>
     </article>
   )
+}
+
+function NewTag({release}){
+return (
+    isNewShoe(release) ? <span className='bg-blue-950 text-white'>Just Released!</span> : null
+)
+}
+
+function SalesTag({sale}){
+    return sale ? <span className='bg-red-300'>Sale</span> : null
+}
+
+function SalesPrice({price}){
+    return (price ? <p>{formatPrice(price)}</p> : null)
+}
+
+function ShoePrice(price){
+    return  <p>{formatPrice(price.price)}</p>
+}
+
+function ColorsP({colors}){
+
+    return <p>{`${colors} ${colors > 1 ? 'Colors' : 'Color'}`}</p>
 }
