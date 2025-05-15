a928c755129b39f919e818cb55f22f54208e8cb5
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-05-14 23:27:17 -0600

    add styles for position the logo and the top navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -3,8 +3,10 @@ import Logo from "../Logo";
 
 export default function Navbar() {
   return (
-    <nav className="border-b-1 border-b-gray-three " >
-      <Logo className={'font-extrabold text-2xl'}/>
+    <nav className="border-b-1 border-b-gray-three relative py-6.5" >
+      <div className=" h-[100%] absolute left-8 flex justify-center items-center top-0">
+        <Logo className={'font-extrabold text-2xl'}/>
+      </div>
       <ol className="flex justify-center gap-5">
         <Link name={'SALE'} className={'text-secondary-blue'}/>
         <Link name={'NEW RELEASES'} />
