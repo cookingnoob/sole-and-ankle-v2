a719c4e078a731b669d6233aaf4c58fbd3e51ddc
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-05-14 23:36:10 -0600

    add styles for superheader, fix typo on css variable

--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -1,5 +1,15 @@
+import Link from "../LinkList";
+
 export default function SuperHeader() {
   return (
-    <div>SuperHeader</div>
+    <div className="bg-gray-nine py-3 text-white flex justify-between">
+      <span className=" ml-8 text-sm">Free shipping on domestic orders over $75!</span>
+      <div className="flex mr-3">
+        <label htmlFor="">
+          <input type="text" placeholder="Search..."/>
+        </label>
+          <Link className={''} name={'Help'}/>
+      </div>
+    </div>
   )
 }
