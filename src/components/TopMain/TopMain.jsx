34e38cff6f55cbbd697a4a09ff2dba50141f765a
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-05-16 17:03:58 -0600

    add styles for label and select tag

--- src/components/TopMain/TopMain.jsx
+++ src/components/TopMain/TopMain.jsx
@@ -1,16 +1,17 @@
 import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
-
+import { ChevronDown } from "react-feather";
 export default function TopMain() {
   return (
         <div className=" flex pb-7 pt-18">
             <Breadcrumbs/>
             <div className="flex justify-between px-7.5 flex-1">
             <h1 className="text-2xl">Running</h1>
-            <label className="">
-              <span>Sort</span>
-              <select name="" id="">
+            <label className="relative">
+              <span className="text-gray-seven text-base pr-3">Sort</span>
+              <select name="" className=" appearance-none bg-gray-one text-gray-nine pt-3 rounded-lg pr-11 pb-3 pl-4">
                 <option value="">Newest Releases</option>
               </select>
+                <ChevronDown className="absolute size-5 right-4 top-3.5"/>
             </label>
             </div>
           </div>
