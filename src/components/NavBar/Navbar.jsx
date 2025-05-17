68250ebfd4a4af315d4120090b960acb96b0ea4e
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-05-16 18:00:34 -0600

    remove one div on the Navbar as per video recommendation, to clean up the Navbar markup. Change the Logo from absolutly positioned to have three containers in the Navbar row and grow the side bars, also as recommended on the video

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -3,11 +3,11 @@ import Logo from "../Logo";
 
 export default function Navbar() {
   return (
-    <nav className="border-b-1 border-b-gray-three relative py-6.5" >
-      <div className=" h-[100%] absolute left-8 flex justify-center items-center top-0">
+    <nav className="border-b-1 border-b-gray-three items-baseline pt-5 h-18 px-8 flex">
+      <div className="flex-1">
         <Logo className={'font-extrabold text-2xl'}/>
-      </div>
-      <ol className="flex justify-center gap-5">
+        </div>
+      <ol className="flex ml-12 gap-12">
         <Link name={'SALE'} className={'text-secondary-blue'}/>
         <Link name={'NEW RELEASES'} />
         <Link name={'MEN'} />
@@ -15,6 +15,7 @@ export default function Navbar() {
         <Link name={'KIDS'} />
         <Link name={'COLLECTIONS'} />
       </ol>
+      <div className="flex-1"></div>
     </nav>
   )
 }
--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -1,15 +1,14 @@
-import Link from "../LinkList";
+
 import Searchbar from "../SearchBar/Searchbar";
 import { ShoppingBag } from "react-feather";
 export default function SuperHeader() {
   return (
-    <div className="bg-gray-nine  text-white flex justify-between">
-      <span className="pl-8 py-3 text-sm">Free shipping on domestic orders over $75!</span>
-      <div className="flex mr-3 py-2 gap-6 text-gray-three">
+    <div className="bg-gray-nine px-8 gap-6 h-11 text-white flex items-center justify-between">
+      <span className="mr-auto text-sm">Free shipping on domestic orders over $75!</span>
+
          <Searchbar/>
-          <a href="" className="">Help</a>
-          <ShoppingBag className="size-6 "/>
-      </div>
+          <a href="" className="text-gray-three">Help</a>
+          <ShoppingBag className="size-6 text-gray-three"/>
     </div>
   )
 }
